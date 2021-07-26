5a60ee6c56f18e829b319d6dfd2b916867279a24
Elliot Nelson <nelson.work@gmail.com>
2021-07-26 09:07:09 -0400

    :bento: initial world assets

---
+++ src/assets/world.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="world" tilewidth="8" tileheight="16" tilecount="256" columns="16">
+ <image source="font-gen.png" trans="000000" width="128" height="256"/>
+</tileset>
